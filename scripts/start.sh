d8cb9abf899ea7cb78e2e77fd5ae50a27e2169a0
Chris Lewis <clewis916@gmail.com>
2024-08-12 18:03:45 -0400

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,11 @@
 cd orchestration
 ./start.sh
+cd ~/experiment_tracking
+poetry env use 3.11.9
+poetry install
+poetry shell
+sleep 2
+poetry run mlflow server -h 0.0.0.0 -p 5000 \
+--backend-store-uri postgresql://<Your AWS RDS Master Username>:<Your AWS RDS Master Password@<Your RDS endpoint>:5432/<Your RDS DB name>  \
+	--artifacts-destination s3:/<Name of your s3 bucket>  \
+	--serve-artifacts" &
